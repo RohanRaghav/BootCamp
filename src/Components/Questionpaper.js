832d04a7e6ee6b573652227d256a66b2b0ff6f1b
Rohan Raghav <132052764+RohanRaghav@users.noreply.github.com>
2024-08-06 10:02:05 +0530

    first commit

--- src/Components/Questionpaper.js
+++ src/Components/Questionpaper.js
@@ -24,7 +24,7 @@ const QuestionPaper = () => {
     useEffect(() => {
         const fetchQuestions = async () => {
             try {
-                const response = await axios.get('https://cac-update.vercel.app/api/questions');
+                const response = await axios.get('https://BootCamp.vercel.app/api/questions');
                 setQuestions(response.data);
                 setAnswers(response.data.map(() => ''));
             } catch (error) {
